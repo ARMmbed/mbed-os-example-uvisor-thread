d7b0b5590c503b6ada2c2fe7bef873ba467fa57d
Jaeden Amero <jaeden.amero@arm.com>
2016-04-14 14:06:21 +0100

    Specify box main thread stack sizes
    Update mbed-os at the same time to get the necessary changes from uVisor
    for the UVISOR_BOX_MAIN macro.

--- source/led2.cpp
+++ source/led2.cpp
@@ -15,7 +15,7 @@ static void led2_main(const void *);
 
 UVISOR_BOX_NAMESPACE(NULL);
 UVISOR_BOX_HEAPSIZE(8192);
-UVISOR_BOX_MAIN(led2_main, osPriorityNormal);
+UVISOR_BOX_MAIN(led2_main, osPriorityNormal, UVISOR_BOX_STACK_SIZE);
 UVISOR_BOX_CONFIG(box_led2, acl, UVISOR_BOX_STACK_SIZE, box_context);
 
 static void led2_main(const void *)
