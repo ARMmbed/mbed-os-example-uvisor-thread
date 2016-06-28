4e2cc1a50315ffc722efe73315c2c66725ae80f1
Niklas Hauser <niklas.hauser@arm.com>
2016-06-28 16:44:39 +0100

    Switch led1 and led3 box

--- source/led1.cpp
+++ source/led1.cpp
@@ -6,10 +6,7 @@
 
 struct box_context {
     Thread * thread;
-    Thread * thread2;
-    Thread * thread3;
     uint32_t heartbeat;
-    int toggle;
 };
 
 static const UvisorBoxAclItem acl[] = {
@@ -22,45 +19,21 @@ UVISOR_BOX_HEAPSIZE(8192);
 UVISOR_BOX_MAIN(led1_main, osPriorityNormal, UVISOR_BOX_STACK_SIZE);
 UVISOR_BOX_CONFIG(box_led1, acl, UVISOR_BOX_STACK_SIZE, box_context);
 
-static void run_1(const void *)
-{
-    while (1) {
-        const int toggle = uvisor_ctx->toggle;
-        uint16_t size = toggle ? 150 : 99;
-        uint16_t seed = (size << 8) | (uvisor_ctx->heartbeat & 0xFF);
-
-        uvisor_ctx->toggle = !uvisor_ctx->toggle;
-        alloc_fill_wait_verify_free(size, seed, 0);
-    }
-}
-
 static void led1_main(const void *)
 {
     DigitalOut led1(LED1);
     led1 = LED_OFF;
-    uvisor_ctx->thread2 = new Thread(run_1);
-    uvisor_ctx->thread3 = new Thread(run_1);
-
-    /* Create page-backed allocator. */
     const uint32_t kB = 1024;
-    SecureAllocator alloc = secure_allocator_create_with_pages(4*kB, 1*kB);
-    /* Prepare the thread definition structure. */
-    osThreadDef_t thread_def;
-    thread_def.stacksize = DEFAULT_STACK_SIZE;
-    /* Allocate the stack inside the page allocator! */
-    thread_def.stack_pointer = (uint32_t *) secure_malloc(alloc, DEFAULT_STACK_SIZE);
-    thread_def.tpriority = osPriorityNormal;
-    thread_def.pthread = &run_1;
-    /* Create a thread with the page allocator as heap. */
-    osThreadContextCreate(&thread_def, NULL, alloc);
 
+    SecureAllocator alloc = secure_allocator_create_with_pages(4*kB, 1*kB);
 
     while (1) {
-        static const size_t size = 20;
+        static const size_t size = 500;
         uint16_t seed = (size << 8) | (uvisor_ctx->heartbeat & 0xFF);
 
         led1 = !led1;
         ++uvisor_ctx->heartbeat;
         alloc_fill_wait_verify_free(size, seed, 211);
+        specific_alloc_fill_wait_verify_free(alloc, 5*kB, seed, 107);
     }
 }
--- source/led3.cpp
+++ source/led3.cpp
@@ -6,7 +6,10 @@
 
 struct box_context {
     Thread * thread;
+    Thread * thread2;
+    Thread * thread3;
     uint32_t heartbeat;
+    int toggle;
 };
 
 static const UvisorBoxAclItem acl[] = {
@@ -19,21 +22,45 @@ UVISOR_BOX_HEAPSIZE(8192);
 UVISOR_BOX_MAIN(led3_main, osPriorityNormal, UVISOR_BOX_STACK_SIZE);
 UVISOR_BOX_CONFIG(box_led3, acl, UVISOR_BOX_STACK_SIZE, box_context);
 
+static void run_3(const void *)
+{
+    while (1) {
+        const int toggle = uvisor_ctx->toggle;
+        uint16_t size = toggle ? 150 : 99;
+        uint16_t seed = (size << 8) | (uvisor_ctx->heartbeat & 0xFF);
+
+        uvisor_ctx->toggle = !uvisor_ctx->toggle;
+        alloc_fill_wait_verify_free(size, seed, 0);
+    }
+}
+
 static void led3_main(const void *)
 {
     DigitalOut led3(LED3);
     led3 = LED_OFF;
-    const uint32_t kB = 1024;
+    uvisor_ctx->thread2 = new Thread(run_3);
+    uvisor_ctx->thread3 = new Thread(run_3);
 
+    /* Create page-backed allocator. */
+    const uint32_t kB = 1024;
     SecureAllocator alloc = secure_allocator_create_with_pages(4*kB, 1*kB);
+    /* Prepare the thread definition structure. */
+    osThreadDef_t thread_def;
+    thread_def.stacksize = DEFAULT_STACK_SIZE;
+    /* Allocate the stack inside the page allocator! */
+    thread_def.stack_pointer = (uint32_t *) secure_malloc(alloc, DEFAULT_STACK_SIZE);
+    thread_def.tpriority = osPriorityNormal;
+    thread_def.pthread = &run_3;
+    /* Create a thread with the page allocator as heap. */
+    osThreadContextCreate(&thread_def, NULL, alloc);
+
 
     while (1) {
-        static const size_t size = 500;
+        static const size_t size = 20;
         uint16_t seed = (size << 8) | (uvisor_ctx->heartbeat & 0xFF);
 
         led3 = !led3;
         ++uvisor_ctx->heartbeat;
-        alloc_fill_wait_verify_free(size, seed, 503);
-        specific_alloc_fill_wait_verify_free(alloc, 5*kB, seed, 100);
+        alloc_fill_wait_verify_free(size, seed, 211);
     }
 }
